6eb6a2c813df4771ffd4fc4c481d402b5dcd48a3
at faghihi <at.faghihi.71@gmailcom>
2022-07-02 06:44:56 -0700

    master

---
+++ index1.js
@@ -0,0 +1,7 @@
+$('#first').html('This is first paragraph');
+$('#second').html('This is second paragraph');
+$('#third').html('This is third paragraph');
+
+$('p').each(function () {
+    $('p').css({ 'background-color': 'pink' });
+});
